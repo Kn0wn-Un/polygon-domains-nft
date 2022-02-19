5db5a66c92832f24c78fef2b28ab8ea50f1f3ac2
Darshan H M <naikdarsh175@gmail.com>
2022-02-19 23:29:58 +0530

    deploy script added

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,22 @@
 const main = async () => {
-	const [owner, randomPerson] = await hre.ethers.getSigners();
 	const domainContractFactory = await hre.ethers.getContractFactory('Domains');
 	const domainContract = await domainContractFactory.deploy('anon');
 	await domainContract.deployed();
-	console.log('Contract deployed to:', domainContract.address);
-	console.log('Contract deployed by:', owner.address);
 
-	let txn = await domainContract.register('Doom', {
+	console.log('Contract deployed to:', domainContract.address);
+
+	let txn = await domainContract.register('Kn0wnUn', {
 		value: hre.ethers.utils.parseEther('0.05'),
 	});
 	await txn.wait();
+	console.log('Minted domain anon.Kn0wnUn');
 
-	const domainAddress = await domainContract.getAddress('Doom');
-	console.log('Owner of domain doom:', domainAddress);
+	txn = await domainContract.setRecord('Kn0wnUn', '>Be me Kn0wnUn');
+	await txn.wait();
+	console.log('Set record for anon.Kn0wnUn');
+
+	const address = await domainContract.getAddress('Kn0wnUn');
+	console.log('Owner of domain Kn0wnUn:', address);
 
 	const balance = await hre.ethers.provider.getBalance(domainContract.address);
 	console.log('Contract balance:', hre.ethers.utils.formatEther(balance));
